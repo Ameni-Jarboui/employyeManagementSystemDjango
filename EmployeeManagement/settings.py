2b09ff8b2785fd077f371c243fd29fd40d026df9
Ameni Jarboui <ameni.jarboui999@gmail.com>
2025-10-23 19:47:45 +0200

    Initial commit

--- EmployeeManagement/settings.py
+++ EmployeeManagement/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'emplopee'
+    'employee'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
